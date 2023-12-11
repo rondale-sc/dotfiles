f9179baae506325139be7cbc3011f3a9f37d7a61
Jon Jackson <jonjacks@jonjacks-mn1.linkedin.biz>
2023-12-11 14:16:09 -0500

    initial commit

---
+++ lua/plugins/core.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "light" -- or 'light'
+
+      vim.cmd.colorscheme("solarized")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          lsp_interop = {
+            enable = true,
+            border = "none",
+            floating_preview_opts = {},
+            peek_definition_code = {
+              ["<leader>df"] = "@function.outer",
+              ["<leader>dF"] = "@class.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
