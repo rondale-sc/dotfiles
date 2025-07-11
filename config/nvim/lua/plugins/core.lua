1895753a47f8f7d4cd4e2b18a37b1a469b64d102
Jonathan Jackson <jonathan.jackson1@me.com>
2025-07-11 16:53:15 -0400

    chore: re-enable and move telescope config to own plugin file

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -51,29 +51,6 @@ return {
       top_down = false,
     },
   },
-  -- {
-  --   "nvim-telescope/telescope.nvim",
-  --   dependencies = {
-  --     {
-  --       "nvim-telescope/telescope-live-grep-args.nvim",
-  --       -- This will not install any breaking changes.
-  --       -- For major updates, this must be adjusted manually.
-  --       version = "^1.0.0",
-  --     },
-  --   },
-  --   config = function()
-  --     local telescope = require("telescope")
-  --
-  --     -- first setup telescope
-  --     telescope.setup({
-  --       -- your config
-  --     })
-  --
-  --     -- then load the extension
-  --     telescope.load_extension("live_grep_args")
-  --   end,
-  -- },
-
   {
     "akinsho/bufferline.nvim",
     config = function()
@@ -86,5 +63,4 @@ return {
       })
     end,
   },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 }
